02deb04a8ffec6c4eaeaf2c739f568672d61765f
matt ashby <matt@ashbys.nz>
2019-08-08 22:07:07 +1200

    update vars

--- variables.tf
+++ variables.tf
@@ -12,12 +12,6 @@ variable "region" {
 Azure PaaS Module Variables - https://github.com/mashbynz/tf-mod-azure-paas
 *****/
 
-variable "loganalytics_name" {
-  type        = string
-  description = ""
-  default     = ""
-}
-
 variable "log_analytics_sku" {
   type        = string
   description = ""
