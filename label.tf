e634e6600a7b64620bc8b31a10751a41faba71db
matt ashby <matt@ashbys.nz>
2019-08-08 22:12:23 +1200

    var

--- label.tf
+++ label.tf
@@ -11,7 +11,7 @@ module "log_analytics_workspace" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
   context            = var.context
   # attributes         = ["analytics"]
-  attributes         = var.loganalytics_name
+  attributes         = [var.loganalytics_name]
   label_order        = ["attributes"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
